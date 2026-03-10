ed32bd8e56a259e22522be89bf4bea51be4aff80
Mira <miradaher@gmail.com>
2026-03-10 16:25:11 +0200

    fix cookie sameSite

--- server/routes/googleAuth.ts
+++ server/routes/googleAuth.ts
@@ -37,7 +37,7 @@ router.get(
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax', // 'lax' needed for cross-origin OAuth redirects
+        sameSite: 'none', // 'lax' needed for cross-origin OAuth redirects
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
